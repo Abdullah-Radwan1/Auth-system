253b8b56ea848ba68dc8400eb15ea9d9fb68a0f1
Abdallah <abdallahbeedo855@gmail.com>
2024-10-09 18:21:41 +0300

    finishing

--- server/utils/generateTandSetC.js
+++ server/utils/generateTandSetC.js
@@ -1,13 +1,16 @@
 import jwt from "jsonwebtoken";
 
 export const generateTandSetC = (res, userId) => {
- const token = jwt.sign({ userId }, process.env.jWT_SECRET, { expiresIn: "1d" });
+ const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+  expiresIn: "7d",
+ });
 
  res.cookie("token", token, {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: "strict",
-  maxAge: 15 * 24 * 60 * 1000,
+  maxAge: 7 * 24 * 60 * 60 * 1000,
  });
+
  return token;
 };
