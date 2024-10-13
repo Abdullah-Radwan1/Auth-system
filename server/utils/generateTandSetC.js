a5de40bbe578c698b0fe5efe02d7d1e6d09c62ea
Abdallah <abdallahbeedo855@gmail.com>
2024-10-13 21:12:53 +0300

    fixing cookies

--- server/utils/generateTandSetC.js
+++ server/utils/generateTandSetC.js
@@ -1,12 +1,12 @@
 import jwt from "jsonwebtoken";
 
-export const generateTandSetC = (res, userId) => {
+export const generateTandSetC = async (res, userId) => {
  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
   expiresIn: "7d",
  });
-
+ // store in the cookies the toekn
  res.cookie("token", token, {
-  httpOnly: true,
+  httpOnly: process.env.NODE_ENV === "production",
   secure: process.env.NODE_ENV === "production",
   sameSite: "strict",
   maxAge: 7 * 24 * 60 * 60 * 1000,
