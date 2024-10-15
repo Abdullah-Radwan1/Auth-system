2c4d9438b2f5ed8b90dd638554e3880c30e4061f
Abdallah <abdallahbeedo855@gmail.com>
2024-10-15 15:20:26 +0300

    auth

--- server/controllers/auth_controller.js
+++ server/controllers/auth_controller.js
@@ -91,23 +91,20 @@ export const verification = async (req, res) => {
  }
 };
 
-export const login = async (req, res) => {
+export const logIn = async (req, res) => {
  const { email, password } = req.body;
  try {
   const user = await User.findOne({ email });
   if (!user) {
    return res.status(400).json({ success: false, message: "Invalid credentials" });
   }
-  const isPasswordValid = await bcryptjs.compare(password, user.password);
-  if (!isPasswordValid) {
+  const isPasswordMatch = await bcryptjs.compare(password, user.password);
+  if (!isPasswordMatch) {
    return res.status(400).json({ success: false, message: "Invalid credentials" });
   }
-
-  generateTokenAndSetCookie(res, user._id);
-
+  generateTandSetC(res, user._id);
   user.lastLogin = new Date();
   await user.save();
-
   res.status(200).json({
    success: true,
    message: "Logged in successfully",
