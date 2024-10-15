2a195a482e9b7e26471e3044c53d749aa4cb3c0f
Abdallah <abdallahbeedo855@gmail.com>
2024-10-15 17:57:37 +0300

    fixing pagination  + adding order POST

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const CORS = process.env.CORS; // Ensure this is your frontend origin
+const CORS = process.env.CORS || "https://auth-system-taupe.vercel.app"; // Ensure this is your frontend origin
 
 // Initialize express app
 const app = express();
@@ -18,11 +18,8 @@ const app = express();
 // CORS Middleware
 app.use(
  cors({
-  origin: CORS, // Use the frontend origin from environment variables
+  origin: CORS, // Use the frontend origin from environment variables  "https://auth-system-taupe.vercel.app"
   credentials: true, // Allow credentials such as cookies to be sent
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Explicitly allow these methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
-  preflightContinue: true,
  }),
 );
 
@@ -31,9 +28,6 @@ app.use(express.json());
 app.use(helmet());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-// Preflight request handling (OPTION method)
-app.options("*", cors()); // Allow preflight requests for all routes
 
 // Mount the routes
 app.use("/auth", authRoutes);
